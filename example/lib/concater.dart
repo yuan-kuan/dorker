5c2d2b8cc5deba25d4485757afc3470c335eb679
kuan <yuankuan@gmail.com>
2018-12-15 14:03:17 +0800

    Example to use dorker

---
+++ example/lib/concater.dart
@@ -0,0 +1,11 @@
+import 'package:dorker/dorker.dart';
+
+class Concater {
+  var _memory = '';
+  Concater(Dorker _boss) {
+    _boss.onMessage.listen((data) {
+      _memory += data;
+      _boss.postMessage.add(_memory);
+    });
+  }
+}
---
+++ example/main.dart
@@ -0,0 +1,32 @@
+import 'dart:html';
+import 'dart:convert';
+
+import 'package:dorker/dorker.dart';
+
+import 'lib/concater.dart';
+
+void main() {
+  Dorker _concatDorker;
+  if (const String.fromEnvironment('USE_WORKER') == 'true') {
+    print('Asked to use worker');
+    _concatDorker = DorkerWorker(Worker('worker/concater_worker.dart.js'));
+  } else 
+  {
+    print('Not using worker');
+    _concatDorker = Dorker();
+    Concater(Dorker.CrossLink(_concatDorker));
+  }
+
+  var input = querySelector('#input') as TextInputElement;
+  var concatButton = querySelector('#btConcat') as ButtonElement;
+  
+  concatButton.onClick.listen((_) {
+    _concatDorker.postMessage.add(input.value);
+  });
+
+  _concatDorker.onMessage.listen((data){
+    querySelector('#output').text = 'Concated: $data';
+  });
+
+  querySelector('#output').text = 'Use the input and button.';
+}
